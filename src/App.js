68af0331dd1b766911f8fd25814845e65b125efd
shucheng <815212637@qq.com>
2019-07-24 15:05:07 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Home from "./pages/Home/index.jsx";
+import Cart from "./pages/Cart/index.jsx";
+import Mine from "./pages/Mine/index.jsx";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+require('./App.css');
+require('./iconfont/iconfont.css');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let AllRouter = {
+  Home,
+  Cart,
+  Mine
+}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          name: "Home",
+          path: "/home",
+          title: "首页",
+          icon: "iconfont icon-shouye"
+        },
+        {
+          name: "Cart",
+          path: "/cart",
+          title: "购物车",
+          icon: "iconfont icon-gouwuchekong"
+        },
+        {
+          name: "Mine",
+          path: "/mine",
+          title: "我的",
+          icon: "iconfont icon-wode"
+        }
+
+      ],
+      isSelect: 0
+    }
+    this.goto = this.goto.bind(this)
+  }
+  goto(path, e) {
+    let { history } = this.props
+    history.push(path)
+  }
+  render() {
+    let { data } = this.state
+    return (
+      <div className="App" id="App">
+        <Switch>
+          {
+            data.map(item => <Route key={item.name} path={item.path} component={AllRouter[item.name]} />)
+          }
+          <Redirect from="/" to="/home" exact />
+        </Switch>
+        <ul className="Appul">
+          {
+            data.map((item, i) => {
+              return <li className='Appli' key={item.name} onClick={this.goto.bind(this, item.path)}>
+                <i className={item.icon}></i>
+                <span> {item.title}</span>
+              </li>
+
+            })
+          }
+        </ul>
+
+      </div>
+    );
+  }
 }
 
+App = withRouter(App);
 export default App;
